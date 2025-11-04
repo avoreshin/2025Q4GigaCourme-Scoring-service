ef90c714c3688080fdc16b0ed85ad95ef62e20c9
avoreshin <avoreshin@gmail.com>
2025-11-04 11:22:43 +0300

    Add nixpacks configuration

---
+++ Documents/breketnn/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
+import {initFlowbite} from 'flowbite';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgOptimizedImage],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'breketnn';
+
+  ngOnInit() {
+    initFlowbite();
+  }
+}
